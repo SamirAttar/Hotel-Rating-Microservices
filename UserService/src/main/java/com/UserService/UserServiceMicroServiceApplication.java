53b67b75cb19391f242dbf18f195904d0e708c85
Samir <attarsamir06@gmail.com>
2023-11-11 16:47:26 +0530

    UserService Microservice

---
+++ UserService/src/main/java/com/UserService/UserServiceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.UserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceMicroServiceApplication.class, args);
+	}
+
+}
