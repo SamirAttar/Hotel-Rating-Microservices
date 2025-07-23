14b43a96d1161a0a8bc9f3558b20bf883823b664
Samir <attarsamir06@gmail.com>
2025-07-23 12:22:25 +0530

    updated

--- UserService/src/main/java/com/UserService/UserServiceMicroServiceApplication.java
+++ UserService/src/main/java/com/UserService/UserServiceMicroServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
@@ -18,8 +21,10 @@ public class UserServiceMicroServiceApplication {
     }
 
     @Bean
-   @LoadBalanced
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+
 }
